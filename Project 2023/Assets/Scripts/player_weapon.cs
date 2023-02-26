4c5380aa98fb37fab61c4db53f20fb855e23d533
limaox <limaoxgood@gmail.com>
2023-02-26 16:44:29 +0800

    V0.0.4 improve the triangulation

--- Project 2023/Assets/Scripts/player_weapon.cs
+++ Project 2023/Assets/Scripts/player_weapon.cs
@@ -140,7 +140,7 @@ public class player_weapon : MonoBehaviour
         return Vector3.Cross(side1, side2);
     }
 
-    private uint Lshift(uint a, int n){
+    /*private uint Lshift(uint a, int n){
         int nn = 32 - n;
         return (a << n) | (a >> nn);
     }
@@ -150,6 +150,10 @@ public class player_weapon : MonoBehaviour
         uint y = unchecked((uint)(a.y*1000));
         uint z = unchecked((uint)(a.z*1000));
         return Lshift(x, 20) ^ Lshift(y, 10) ^ Lshift(z, 0);
+    }*/
+    private string hash(Vector3 a){
+        string s = a.x.ToString("F3")+" "+ a.y.ToString("F3") + " " + a.z.ToString("F3");
+        return s;
     }
 
     //can't use disjoint set to count number of group that are unconnect
@@ -190,7 +194,7 @@ public class player_weapon : MonoBehaviour
         if(group.Count == 1){
             for(int i = 0; i < e.vertices.Count; ++i){
                 Vector3 vertex = e.vertices[i];
-                uint key = hash(vertex);
+                string key = hash(vertex);
             }
             objs.Add(e);
         }else{
@@ -203,7 +207,7 @@ public class player_weapon : MonoBehaviour
                 for(int j = 0; j < 3; ++j){
                     int index = e.triangles[i + j];
                     Vector3 vertex = e.vertices[index];
-                    uint planeKey = hash(vertex);
+                    string planeKey = hash(vertex);
 
                     vertices.Add(vertex);
                     uvs.Add(e.uvs[index]);
@@ -224,7 +228,7 @@ public class player_weapon : MonoBehaviour
         return group.Count;
     }
 
-    private void fillGap(Element e, Element e1, Dictionary<uint, Vector3> planevertex, Dictionary<uint, Dictionary<uint, bool>> edges, Plane plane){
+    private void fillGap(Element e, Element e1, Dictionary<string, Vector3> planevertex, Dictionary<string, HashSet<string>> edges, Plane plane){
         if(planevertex.Count < 3)
             return;
         //List<Vector3> triangles = DT.bowyer_watson(e.planevertex, plane);
@@ -253,7 +257,6 @@ public class player_weapon : MonoBehaviour
             g1.bws = bw; g2.bws = bw;
             g1.vertices = vertex1; g1.normals = normal2;
             g2.vertices = vertex2; g2.normals = normal1;
-            
             if(e.face && direction >= 0 || !e.face && direction < 0){
                 add_mesh(e, g1, false);
                 add_mesh(e1, g2, false);
@@ -422,8 +425,8 @@ public class player_weapon : MonoBehaviour
         if(skin) BWs = sharedMesh.boneWeights;
         else BWs = new BoneWeight[0];
 
-        Dictionary<uint, Vector3> vertexOnPlane = new Dictionary<uint, Vector3>();
-        Dictionary<uint, Dictionary<uint, bool>> edges = new Dictionary<uint, Dictionary<uint, bool>>();
+        Dictionary<string, Vector3> vertexOnPlane = new Dictionary<string, Vector3>();
+        Dictionary<string, HashSet<string>> edges = new Dictionary<string, HashSet<string>>();
 
         for(int i = 0; i < meshTriangles.Length; i += 3){
             //在這邊面是由三角形組成, 三角形又是由三個點組成的,所以說
@@ -496,14 +499,14 @@ public class player_weapon : MonoBehaviour
                         add_meshSide(vSide[v2],  positive, negative, g3, true);
                     }
                 }
-                uint key = hash(intersectionPoint[0]),key1 = hash(intersectionPoint[1]);
+                string key = hash(intersectionPoint[0]),key1 = hash(intersectionPoint[1]);
 
                 if(!vertexOnPlane.ContainsKey(key)) vertexOnPlane.Add(key, intersectionPoint[0]);
                 if(!vertexOnPlane.ContainsKey(key1)) vertexOnPlane.Add(key1, intersectionPoint[1]);
-                if(!edges.ContainsKey(key)) edges.Add(key, new Dictionary<uint, bool>());
-                if(!edges.ContainsKey(key1)) edges.Add(key1, new Dictionary<uint, bool>());
-                if(!edges[key].ContainsKey(key1) && key != key1) edges[key].Add(key1, false);
-                if(!edges[key1].ContainsKey(key) && key != key1) edges[key1].Add(key, false);
+                if(!edges.ContainsKey(key)) edges.Add(key, new HashSet<string>());
+                if(!edges.ContainsKey(key1)) edges.Add(key1, new HashSet<string>());
+                if(!edges[key].Contains(key1) && key != key1) edges[key].Add(key1);
+                if(!edges[key1].Contains(key) && key != key1) edges[key1].Add(key);
             }
         }
         List<Element> objs = new List<Element>();
@@ -512,8 +515,11 @@ public class player_weapon : MonoBehaviour
         negative.face = false;
         fillGap(positive, negative, vertexOnPlane, edges, plane);
 
-        disjointSet_split(positive, objs);
-        disjointSet_split(negative, objs);
+        int groupNumber = 0;
+        groupNumber = Mathf.Max(disjointSet_split(positive, objs), groupNumber);
+        groupNumber = Mathf.Max(disjointSet_split(negative, objs),groupNumber);
+
+        Debug.Log("group_num: " + groupNumber);
 
         objs.Sort((a, b) => {
             int aVN = a.vertices.Count;
