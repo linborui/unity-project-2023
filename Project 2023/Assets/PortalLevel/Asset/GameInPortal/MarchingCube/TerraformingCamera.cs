1e9d1945472733bb99807df4373b5a5efc3cec7e
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2023-05-27 00:40:41 +0800

    V0.0.8(Add ChatGPT NPC)

--- Project 2023/Assets/PortalLevel/Asset/GameInPortal/MarchingCube/TerraformingCamera.cs
+++ Project 2023/Assets/PortalLevel/Asset/GameInPortal/MarchingCube/TerraformingCamera.cs
@@ -10,42 +10,36 @@ public class TerraformingCamera : MonoBehaviour
 
 	public float BrushSize = 2f;
 	RaycastHit prevhit;
-	Portal[] portals;
-	int times;
+
     void Awake () {
 		_cam = GetComponent<Camera>();
-        portals = FindObjectsOfType<Portal> ();
 		_hitPrevPoint = _hitPoint = Vector3.zero;
-		times = 0;
     }
 
-	private void LateUpdate() {
+	private void Update() {
 		if(InputManager.GetButtonDown("PortalIn")) {
 			Terraform(true);
 		}
 		else if (InputManager.GetButtonDown("PortalOut")) {
 			Terraform(false);
 		}
-		times ++ ;
 	}
 
 	private void Terraform(bool add) {
 		RaycastHit hit;
 
-		if (Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hit, 1000)) {
+		if (Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
 			Chunk hitChunk = hit.collider.gameObject.GetComponent<Chunk>();
 			if(hitChunk == null)
 				return;
 			_hitPoint = hit.point;
-			float mouseX = Input.mousePosition.x;
-			float mouseY = Input.mousePosition.y;
-			
+			/*
 			if(_hitPrevPoint == Vector3.zero)
 				_hitPrevPoint = hit.point;
 			else{
 				_hitPoint = new Vector3( (_hitPoint.x + _hitPrevPoint.x)/2,( _hitPoint.y + _hitPrevPoint.y)/2, (_hitPoint.z + _hitPrevPoint.z)/2);
 				_hitPrevPoint = hit.point;
-			}
+			}*/
 			hitChunk.EditWeights(_hitPoint, BrushSize, add);
 		}
 	}
